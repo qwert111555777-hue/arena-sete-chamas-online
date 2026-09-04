1f648646dc71ea4492a6a81be1145352503a11c8
Arena Dev <dev@arena.local>
2026-09-04 03:59:10 +0000

    v23: compacta imagens dos rostos (melhor desempenho no celular).

--- public/client.js
+++ public/client.js
@@ -10,7 +10,7 @@ let game = null;
 let openRooms = [];
 let currentScreen = 'menuScreen';
 let toastTimer = null;
-const ASSET_VERSION = '22';
+const ASSET_VERSION = '23';
 const SHOW_ARENA_TEXT = false;
 const SHOW_STAGE_INTRO = true;
 
